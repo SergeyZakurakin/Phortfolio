690d9683578275d77e94d33d75776b719cc1b7ee
SergeyZakurakin <pricezak@gmail.com>
2025-01-11 21:55:53 -0500

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Sergey Zakurakin on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
